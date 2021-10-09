dd458c94ac1939c37d3cd247280056d20844b572
Juan <juanan0309@gmail.com>
2021-10-08 22:21:08 -0500

    Add toggle option

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,33 @@ import "./App.css";
 import TodoList from "./components/TodoList";
 import ToDoCreate from "./components/ToDoCreate";
 
+type taskState = {
+  title: string;
+  completed: boolean;
+};
+
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<taskState[]>([]);
 
   const handleCreate = (task: string) => {
-    setTasks(tasks => [...tasks, task]);
+    setTasks(tasks => [...tasks, { title: task, completed: false }]);
   };
 
   const handleDelete = (idx: number) => {
     setTasks(tasks => tasks.filter((_, id) => idx !== id));
   };
 
+  const handleToggle = (idx: number) => {
+    setTasks(tasks =>
+      tasks.map((task, id) =>
+        idx !== id ? task : { ...task, completed: !task.completed }
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <TodoList tasks={tasks} delete={handleDelete} />
+      <TodoList tasks={tasks} delete={handleDelete} toggle={handleToggle} />
       <ToDoCreate create={handleCreate}></ToDoCreate>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,32 @@
 import "../assets/styles/ToDoList.css";
+type taskState = {
+  title: string;
+  completed: boolean;
+};
 
 type TodoListProps = {
-  tasks: String[];
+  tasks: taskState[];
   delete: (idx: number) => void;
+  toggle: (idx: number) => void;
 };
 
 const TodoList = (props: TodoListProps) => {
   const handleDelete = (idx: number) => {
     props.delete(idx);
   };
+
+  const handleToggle = (idx: number) => {
+    props.toggle(idx);
+  };
+
   return (
     <div>
       <ul>
         {props.tasks.map((task, idx) => (
-          <li key={`${task}${idx}`}>
-            {task}
+          <li className="listItem" key={`${task.title}${idx}`}>
+            <div className={`${task.completed === true ? "crossed" : ""}`}>
+              <p onClick={() => handleToggle(idx)}>{task.title}</p>
+            </div>
             <span className="deleteButton" onClick={() => handleDelete(idx)}>
               X
             </span>
